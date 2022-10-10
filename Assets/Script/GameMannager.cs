9d7d76db35696a32e8103fed8de13af0fe3afe69
李庠霖 <snowmelt945@gmail.com>
2022-10-10 19:49:24 +0800

    沒上傳到設定

---
+++ Assets/Script/GameMannager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMannager : MonoBehaviour
+{
+    [SerializeField]
+    public static CloakSystem cloakSystem;
+}
