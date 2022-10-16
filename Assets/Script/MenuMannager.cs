18d8ba00ec6217534aa994277074380469c9191e
李庠霖 <snowmelt945@gmail.com>
2022-10-17 00:05:02 +0800

    遊戲開始畫面及場景切換

---
+++ Assets/Script/MenuMannager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMannager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Setting()
+    {
+        Debug.Log("開啟設定選單");
+    }
+
+    public void Credit()
+    {
+        Debug.Log("開啟製作人員");
+    }
+}
