4bb90d0cf541630c9d7045ebd962b6bedea9adbd
李庠霖 <snowmelt945@gmail.com>
2022-12-18 15:19:17 +0800

    2022.12.18 新增 遊戲開始 Opening 動畫 更新 遊戲完整流程 [匯入星塔、傳送門]

---
+++ Assets/Script/OpeningScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningScript : MonoBehaviour
+{
+    public void StartGameEEEEEEEEEEEE()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
